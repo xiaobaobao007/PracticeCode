e4510e8796a49f2426d73ffc6aa5f4014a902068
xiaobaobao007 <932824098@qq.com>
2019-06-24 10:13:27 +0800

    比较器和线程池

---
+++ src/Compare/Main.java
@@ -0,0 +1,25 @@
+package Compare;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * @author xiaobaobao
+ * @date 2019/6/24 9:36
+ */
+public class Main {
+    public static void main(String[] args) {
+        List<Person> personList = new ArrayList<>();
+        Random random = new Random();
+        for (int i = 0; i < 10; i++) {
+            personList.add(new Person(random.nextInt(1000)));
+        }
+//        Collections.sort(personList, (o1, o2) -> o2.getAge() - o1.getAge());
+        Collections.sort(personList);
+        for (Person person : personList) {
+            System.out.println(person.getAge());
+        }
+    }
+}
--- src/ThreadProblem/Main.java
+++ src/ThreadProblem/Main.java
@@ -2,9 +2,7 @@ package ThreadProblem;
 
 public class Main {
     public static void main(String[] args) {
-        thread t = new thread("1");
-        new Thread(t).start();
-        new Thread(t).start();
-        new Thread(t).start();
+        threadFactory factory = new threadFactory();
+        factory.newThread(new thread("1"));
     }
 }

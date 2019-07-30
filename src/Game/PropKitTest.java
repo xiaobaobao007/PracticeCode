69920e1d0464c0ed78c112c401fc838a16258854
xiaobaobao007 <932824098@qq.com>
2019-07-30 11:22:09 +0800

    jfinal的propkit读取配置文件方式

---
+++ src/Game/PropKitTest.java
@@ -0,0 +1,18 @@
+package Game;
+
+import com.jfinal.kit.Prop;
+import com.jfinal.kit.PropKit;
+
+/**
+ * @author xiaobaobao
+ * @date 2019/7/30 10:55
+ */
+public class PropKitTest {
+    public static void main(String[] args) {
+        PropKit.use("Game/server.conf");
+        String id = PropKit.get("server.id");
+        System.out.println(id);
+        String bmy = PropKit.get("fasfasdf","no key");
+        System.out.println(bmy);
+    }
+}

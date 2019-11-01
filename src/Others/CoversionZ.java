cc7e5d5c171149697c6ec5da911fd3927c4f3483
xiaobaobao007 <932824098@qq.com>
2019-11-01 10:43:47 +0800

    字符串转Z输出,考察集合和StringBulider的应用

---
+++ src/Others/CoversionZ.java
@@ -0,0 +1,51 @@
+package Others;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 输入: s = "LEETCODEISHIRING", numRows = 4
+ * 输出: "LDREOEIIECIHNTSG"
+ * 解释:
+ * <p>
+ * L     D     R
+ * E   O E   I I
+ * E C   I H   N
+ * T     S     G
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/zigzag-conversion
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class CoversionZ {
+
+
+	public static void main(String[] args) {
+		System.out.println(new CoversionZ().convert("AB", 1));
+	}
+
+	public String convert(String s, int numRows) {
+		if (numRows==1) {
+			return s;
+		}
+
+		List<StringBuilder> rows = new ArrayList<>();
+		for (int i = 0; i < Math.min(numRows, s.length()); i++) {
+			rows.add(new StringBuilder());
+		}
+
+		int n = 0, flag = 1;
+		for (int i = 0; i < s.length(); i++) {
+			rows.get(n).append(s.charAt(i));
+			n += flag;
+			if (n == 0 || n == numRows - 1) {
+				flag *= -1;
+			}
+		}
+		StringBuilder sb = new StringBuilder();
+		for (StringBuilder ss : rows) {
+			sb.append(ss);
+		}
+		return sb.toString();
+	}
+}

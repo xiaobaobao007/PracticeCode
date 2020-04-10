4c4b302ef0ae31e93ece81ac056c04e912f13d64
xiaobaobao007 <932824098@qq.com>
2020-04-10 23:31:04 +0800

    线程安全交换数据

--- src/ClassProblem/Test.java
+++ src/ClassProblem/Test.java
@@ -1,6 +1,6 @@
 package ClassProblem;
 
-import java.lang.reflect.Constructor;
+import java.util.HashMap;
 
 /**
  * 类加载的顺序
@@ -8,14 +8,19 @@ import java.lang.reflect.Constructor;
 public class Test {
 	public static void main(String[] ars) throws IllegalAccessException, InstantiationException, NoSuchMethodException, ClassNotFoundException {
 
-		new ChildrenTest();//执行到此处,结果: 1a1b2a2b
-		System.out.println();
-		AbstractTest ab = new ChildrenTest();//执行到此处,结果: 2a2b
-		Class name = Class.forName("ChildrenTest");
-		ChildrenTest childrenTest = (ChildrenTest) name.newInstance();
-		Constructor<ChildrenTest> declaredConstructor = ChildrenTest.class.getDeclaredConstructor();
+//		new ChildrenTest();//执行到此处,结果: 1a1b2a2b
+//		System.out.println();
+//		AbstractTest ab = new ChildrenTest();//执行到此处,结果: 2a2b
+//		Class name = Class.forName("ChildrenTest");
+//		ChildrenTest childrenTest = (ChildrenTest) name.newInstance();
+//		Constructor<ChildrenTest> declaredConstructor = ChildrenTest.class.getDeclaredConstructor();
+//
+//		int a = InterfaceTest.a;
 
-		int a = InterfaceTest.a;
+		HashMap<Object, Object> map = new HashMap<>();
+		for (int i = 16; i < 600; i += 16) {
+			map.put(i, i);
+		}
 
 	}
 }

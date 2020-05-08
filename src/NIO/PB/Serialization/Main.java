3d9fdd2a7ff78af714e918f198c422cd33fed2f9
xiaobaobao007 <932824098@qq.com>
2020-05-08 17:35:36 +0800

    pb和json转换

--- src/NIO/PB/Serialization/Main.java
+++ src/NIO/PB/Serialization/Main.java
@@ -4,7 +4,10 @@ import NIO.PB.HelloWorld.Helloworld;
 import NIO.PB.HelloWorld.Helloworld.HelloRequest;
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
+
 import com.google.protobuf.InvalidProtocolBufferException;
+import com.googlecode.protobuf.format.JsonFormat;
 
 /**
  * @author xiaobaobao
@@ -41,5 +44,23 @@ public class Main {
 
 		builder.setOne1("1");
 		System.out.println(builder.getOne1() + "|" + builder.getOne2());
+
+	}
+
+	@Test
+	public void json_test() throws Exception {
+		JsonFormat jsonFormat = new JsonFormat();
+		Helloworld.allValue.Builder builder = Helloworld.allValue.newBuilder();
+		builder.setInt32Value(123);
+		builder.setFloatValue(1.2F);
+		builder.setStringValue("sdfe");
+		String json = jsonFormat.printToString(builder.build());
+		System.out.println(json);
+
+		Helloworld.allValue.Builder newBuilder = Helloworld.allValue.newBuilder();
+		jsonFormat.merge(new ByteArrayInputStream(json.getBytes()), newBuilder);
+		System.out.println(newBuilder.getInt32Value());
+		System.out.println(newBuilder.getFloatValue());
+		System.out.println(newBuilder.getStringValue());
 	}
 }

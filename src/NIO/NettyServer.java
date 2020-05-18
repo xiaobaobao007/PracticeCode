165590456466b9002b74592508edabfb920b020b
xiaobaobao007 <932824098@qq.com>
2020-05-18 22:39:32 +0800

    netty粘包半包

--- src/NIO/NettyServer.java
+++ src/NIO/NettyServer.java
@@ -10,6 +10,9 @@ import io.netty.channel.socket.nio.NioChannelOption;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.FixedLengthFrameDecoder;
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
+import io.netty.handler.codec.LineBasedFrameDecoder;
 import io.netty.handler.codec.protobuf.ProtobufDecoder;
 import io.netty.handler.codec.protobuf.ProtobufEncoder;
 import io.netty.handler.codec.protobuf.ProtobufVarint32FrameDecoder;
@@ -49,6 +52,27 @@ public class NettyServer {
 						p.addLast(new ProtobufVarint32LengthFieldPrepender());
 						//二次编码器
 						p.addLast(new ProtobufEncoder());
+
+						if (false) {
+							//TODO 粘包半包问题
+							//固定长度
+							p.addLast(new FixedLengthFrameDecoder(100));
+							//分隔符
+							p.addLast(new DelimiterBasedFrameDecoder(Integer.MAX_VALUE,
+									Unpooled.copiedBuffer(System.getProperty("line.separator").getBytes())));
+							//携带length字段信息
+							//byteOrder
+							//maxFrameLength 发送数据帧的最大长度
+							//lengthFieldOffset 长度域起始位置
+							//lengthFieldLength 长度域长度
+							//lengthAdjustment 添加字段的补偿值
+							//initialBytesToStrip 解包抛弃前边多少数据
+							//failFast
+							p.addLast(new LengthFieldBasedFrameDecoder());
+							//行分割
+							p.addLast(new LineBasedFrameDecoder());
+						}
+
 
 						//idle操作
 						p.addLast("IdleStateHandler", new IdleStateHandler(0, 20, 0, TimeUnit.SECONDS));

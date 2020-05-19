d26a602a8b93e4b66cf67b5d453ad70d1603932a
xiaobaobao007 <932824098@qq.com>
2020-05-19 23:45:23 +0800

    netty如何解决粘包半包

--- src/NIO/NettyServer.java
+++ src/NIO/NettyServer.java
@@ -18,7 +18,10 @@ import io.netty.handler.codec.protobuf.ProtobufEncoder;
 import io.netty.handler.codec.protobuf.ProtobufVarint32FrameDecoder;
 import io.netty.handler.codec.protobuf.ProtobufVarint32LengthFieldPrepender;
 import io.netty.handler.timeout.IdleStateHandler;
+import org.junit.Test;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -55,22 +58,32 @@ public class NettyServer {
 
 						if (false) {
 							//TODO 粘包半包问题
+							//分隔符,支持多种分隔符
+							p.addLast(
+									new DelimiterBasedFrameDecoder(Integer.MAX_VALUE, Unpooled.copiedBuffer(System.getProperty("line.separator").getBytes())));
+
 							//固定长度
-							p.addLast(new FixedLengthFrameDecoder(100));
-							//分隔符
-							p.addLast(new DelimiterBasedFrameDecoder(Integer.MAX_VALUE,
-									Unpooled.copiedBuffer(System.getProperty("line.separator").getBytes())));
+							p.addLast(
+									new FixedLengthFrameDecoder(100));
 							//携带length字段信息
-							//byteOrder
+							//byteOrder,LITTLE_ENDIAN先存低位，BIG_ENDIAN先存高位
+
 							//maxFrameLength 发送数据帧的最大长度
 							//lengthFieldOffset 长度域起始位置
 							//lengthFieldLength 长度域长度
-							//lengthAdjustment 添加字段的补偿值
-							//initialBytesToStrip 解包抛弃前边多少数据
-							//failFast
-							p.addLast(new LengthFieldBasedFrameDecoder());
+							//lengthAdjustment 添加字段的补偿值,实际数据和length的间隔
+							//initialBytesToStrip 解包抛弃前边多少数据=lengthFieldLength+lengthAdjustment
+
+							//failFast 是否立即报错
+							p.addLast(
+									new LengthFieldBasedFrameDecoder(Integer.MAX_VALUE, 0, 4, 0, 4));
+
 							//行分割
-							p.addLast(new LineBasedFrameDecoder());
+							//maxLength，最大长度
+							//stripDelimiter，是否剔除分隔符
+							//failFast，是否立即报错
+							p.addLast(
+									new LineBasedFrameDecoder(Integer.MAX_VALUE, true, true));
 						}
 
 
@@ -98,5 +111,25 @@ public class NettyServer {
 			boos.shutdownGracefully();
 			worker.shutdownGracefully();
 		}
+	}
+
+	@Test
+	public void test() {
+		ByteBuffer buf = ByteBuffer.allocate(4);
+		System.out.println(buf.order().toString());
+
+		buf.order(ByteOrder.LITTLE_ENDIAN);
+//		buf.order(ByteOrder.BIG_ENDIAN);
+		buf.putShort((byte) 1);
+//		buf.put((byte) 3);
+//		buf.put((byte) 5);
+//		buf.put((byte) 7);
+
+		buf.flip();
+		for (int i = 0; i < buf.limit(); i++) {
+			System.out.println(buf.get() & 0xFF);
+		}
+
+		System.out.println("My PC: " + ByteOrder.nativeOrder().toString());
 	}
 }

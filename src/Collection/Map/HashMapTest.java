4f8237ccbed1282dc0eb566ac7973c067b5bc46b
xiaobaobao007 <932824098@qq.com>
2020-05-29 10:01:05 +0800

    map的函数式计算

--- src/Collection/Map/HashMapTest.java
+++ src/Collection/Map/HashMapTest.java
@@ -6,29 +6,14 @@ import java.util.Map;
 public class HashMapTest {
 
 	public static void main(String[] argv) {
-		Map<Integer, Integer> string = new HashMap<>();
-		string.put(null, 1);
-		string.put(1, null);
-		string.put(1, 1);
-		put(string, null);
-		for (int i = 0, j = 0; j < 10; i += 100, j++) {
-			put(string, i);
-		}
-		//如果存在0则返回值否则2
-		System.out.println(string.getOrDefault(0, 2));
-		System.out.println(string.getOrDefault(-1, 2));
-		sout(string);
-
-		System.out.println(1 ^ 2);
+		Map<Integer, Integer> map = new HashMap<>();
+		//对key和value存在才计算
+		map.put(0, 100);
+		map.computeIfPresent(0, (k, v) -> k + 10);
+		System.out.println(map.get(0));
+		map.put(0, 100);
+		map.computeIfPresent(0, (k, v) -> v + 10);
+		System.out.println(map.get(0));
 	}
 
-	private static void put(Map<Integer, Integer> map, Integer value) {
-		map.put(value, value);
-	}
-
-	private static void sout(Map<Integer, Integer> map) {
-		for (Object o : map.keySet()) {
-			System.out.println(o + "," + map.get(o));
-		}
-	}
 }

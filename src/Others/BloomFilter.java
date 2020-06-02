4a534e693b921e5e31c3172dbbaadfb42248fa27
xiaobaobao007 <932824098@qq.com>
2020-06-02 23:03:45 +0800

    布隆过滤器

---
+++ src/Others/BloomFilter.java
@@ -0,0 +1,75 @@
+package Others;
+
+import java.util.BitSet;
+
+/**
+ * @author xiaobaobao
+ * @date 2020/6/2，22:43
+ * <p>
+ * 布隆过滤器
+ */
+public class BloomFilter {
+	private static final int DEFAULT_SIZE = 2 << 24;
+	private static final int[] seeds = new int[]{5, 7, 11, 13, 31, 37, 61};
+	private BitSet bits = new BitSet(DEFAULT_SIZE);
+	private SimpleHash[] func = new SimpleHash[seeds.length];
+
+	public BloomFilter() {
+		for (int i = 0; i < seeds.length; i++) {
+			func[i] = new SimpleHash(DEFAULT_SIZE, seeds[i]);
+		}
+	}
+
+	public void add(String value) {
+		for (SimpleHash f : func) {
+			bits.set(f.hashAndGetIndex(value), true);
+		}
+	}
+
+	public boolean contains(String value) {
+		if (value == null) {
+			return false;
+		}
+		for (SimpleHash f : func) {
+			if (!bits.get(f.hashAndGetIndex(value))) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	// 内部类，simpleHash
+	public static class SimpleHash {
+
+		private int cap;
+		private int seed;
+
+		public SimpleHash(int cap, int seed) {
+			this.cap = cap;
+			this.seed = seed;
+		}
+
+		public int hashAndGetIndex(String value) {
+			int result = 0;
+			int len = value.length();
+			for (int i = 0; i < len; i++) {
+				result = seed * result + value.charAt(i);
+			}
+			return (cap - 1) & result;
+		}
+	}
+
+	public static void main(String[] args) {
+		BloomFilter bf = new BloomFilter();
+		bf.add("rewc");
+		bf.add("test");
+		bf.add("tefdsafst");
+		bf.add("vzcv");
+		bf.add("ewfa");
+		bf.add("tesvart");
+
+		System.out.println(bf.contains("343"));
+		System.out.println(bf.contains("vzcv"));
+	}
+
+}

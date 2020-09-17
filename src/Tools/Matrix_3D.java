14bcf6cc17702d2341ad90a50585ab9efa8fe4fe
xiaobaobao007 <932824098@qq.com>
2020-09-17 13:26:11 +0800

    三维视图颜色版

--- src/Tools/Matrix_3D.java
+++ src/Tools/Matrix_3D.java
@@ -9,22 +9,34 @@ public class Matrix_3D extends JPanel {
 
 	public static JFrame jFrame;
 
+	//正方体边长的一半长度
 	static double length = 300.0;
+	//正方体坐标
 	static double[][] points = {{-length, -length, length}, {length, -length, length}, {length, length, length}, {-length, length, length},
 			{-length, -length, -length}, {length, -length, -length}, {length, length, -length}, {-length, length, -length}};
+	//正方体的面
+	static int[][] side = {
+			{0, 1, 2, 3},
+			{3, 2, 6, 7},
+			{2, 6, 5, 1},
+			{1, 5, 4, 0},
+			{0, 4, 7, 3},
+			{4, 5, 6, 7}};
+	//每个面对应的颜色
+	static Color[] colors = {Color.RED, Color.ORANGE, Color.BLUE, Color.YELLOW, Color.MAGENTA, Color.GREEN};
 	//投影平面的中心点
 	static double[] canvas = {0, 500, 500};
 	//投影平面的法向量
 	static double[] canvas_c = {0, 100, 100};
 	//投影平面的上的一点，此点到canvas视为投影平面的最上方
 	static double[] canvas_p = {0, 400, 600};
-	//摄像机位置
-	static double[] eye = {0, 600, 600};
-
+	//摄像机向量
+	static double[] eye = {0, 1000, 1000};
 	//计算结果缓存
 	double[][] d = new double[8][3];
 	//立方体的各个面的法向量
 	double[] o_o = new double[3];
+	//缓存二维坐标
 	int[][] b = new int[8][2];
 	int center_x = 500;
 	int center_y = 500;
@@ -33,6 +45,7 @@ public class Matrix_3D extends JPanel {
 
 	enum XYZ {
 
+		//起始旋转角度和变化量
 		X(0, 0.1),
 		Y(0, 0.1),
 		Z(0, 0.1);
@@ -60,6 +73,10 @@ public class Matrix_3D extends JPanel {
 		}
 	}
 
+	public static void main(String[] args) {
+		new Matrix_3D();
+	}
+
 	public Matrix_3D() {
 
 		jFrame = new JFrame();
@@ -80,6 +97,7 @@ public class Matrix_3D extends JPanel {
 		XYZ.Y.init();
 		XYZ.Z.init();
 
+		//另起线程
 		new Thread(() -> {
 			for (; ; ) {
 				try {
@@ -87,91 +105,60 @@ public class Matrix_3D extends JPanel {
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
+				//角度改变
 				XYZ.X.change();
 				XYZ.Y.change();
 				XYZ.Z.change();
+				//绘图
 				this.repaint();
 			}
 		}).start();
 	}
 
-	public static void main(String[] args) {
-		new Matrix_3D();
-	}
-
 	@Override
 	public void paint(Graphics g) {
 		super.paint(g);
-
+		//计算八个点投影后的坐标
 		for (int i = 0; i < 8; i++) {
-			get(i);
+			calcute_3d_2d(i);
 		}
-
-		calcute_o_o(g, 0, 1, 2, 3);
-		calcute_o_o(g, 3, 2, 6, 7);
-		calcute_o_o(g, 2, 6, 5, 1);
-		calcute_o_o(g, 1, 5, 4, 0);
-		calcute_o_o(g, 0, 4, 7, 3);
-		calcute_o_o(g, 4, 5, 6, 7);
-
-		hadPoint = 0;
-		hadPoint_1 = 0;
+		//绘图
+		for (int i = side.length - 1; i >= 0; i--) {
+			calcute_o_o(g, i, side[i]);
+		}
 	}
 
-	static int hadPoint = 0;
-	static int hadPoint_1 = 0;
+	//变量外定义，避免重复定义
+	int[] xx = new int[4];
+	int[] yy = new int[4];
 
-	static int had = 0;
-	static int had_1 = 0;
-
-	/**
-	 * 画过的线不会画第二遍
-	 */
-	public static boolean judgeAndPoint(int a, int b) {
-		if (a > b) {
-			had = (hadPoint | (1 << a));
-			had_1 = hadPoint_1 | (1 << b);
-		} else {
-			had = (hadPoint | (1 << b));
-			had_1 = hadPoint_1 | (1 << a);
-		}
-		if (had == hadPoint_1 && had_1 == hadPoint) {
-			return false;
-		}
-		hadPoint = had;
-		hadPoint_1 = had_1;
-		return true;
-	}
-
-	public void calcute_o_o(Graphics g, int a1, int a2, int a3, int a4) {
+	public void calcute_o_o(Graphics g, int index, int[] a1) {
 		for (int i = 0; i < 3; i++) {
-			o_o[i] = (d[a1][i] + d[a2][i] + d[a3][i] + d[a4][i]) / 4;
+			o_o[i] = (d[a1[0]][i] + d[a1[1]][i] + d[a1[2]][i] + d[a1[3]][i]) / 4;
 		}
 		int j = 0;
 		for (int i = 0; i < 3; i++) {
 			j += o_o[i] * (eye[i] - o_o[i]);
 		}
 		if (j >= 0) {
-			if (judgeAndPoint(a1, a2)) {
-				paint___(g, a1, a2);
-			}
-			if (judgeAndPoint(a2, a3)) {
-				paint___(g, a2, a3);
-			}
-			if (judgeAndPoint(a3, a4)) {
-				paint___(g, a3, a4);
-			}
-			if (judgeAndPoint(a4, a1)) {
-				paint___(g, a4, a1);
-			}
+			//平面法向量与视线夹角小于90度
+			xx[0] = center_x + b[a1[0]][0];
+			xx[1] = center_x + b[a1[1]][0];
+			xx[2] = center_x + b[a1[2]][0];
+			xx[3] = center_x + b[a1[3]][0];
+
+			yy[0] = center_y + b[a1[0]][1];
+			yy[1] = center_y + b[a1[1]][1];
+			yy[2] = center_y + b[a1[2]][1];
+			yy[3] = center_y + b[a1[3]][1];
+
+			g.setColor(colors[index]);
+			g.fillPolygon(xx, yy, 4);
 		}
 	}
 
-	public void paint___(Graphics g, int i, int j) {
-		g.drawLine(center_x + b[i][0], center_y + b[i][1], center_x + b[j][0], center_y + b[j][1]);
-	}
-
-	public void get(int index) {
+	//计算3d点到2d点
+	public void calcute_3d_2d(int index) {
 		d[index][0] = points[index][0];
 		d[index][1] = points[index][1] * XYZ.X.cos - points[index][2] * XYZ.X.sin;
 		d[index][2] = points[index][1] * XYZ.X.sin + points[index][2] * XYZ.X.cos;
@@ -183,27 +170,34 @@ public class Matrix_3D extends JPanel {
 		d[index][1] = qwe * XYZ.Z.sin + d[index][1] * XYZ.Z.cos;
 		d[index][0] = rty;
 
-		double[] t = aaaaCalPlaneLineIntersectPoint(d[index]);
+		double[] t = calPlaneLineIntersectPoint(d[index]);
 		if (t == null) {
+			//平行
 			return;
 		}
 
+		//计算三条边的长度
 		double o = gerDistance(t, canvas_p);
 		double p = gerDistance(canvas_p, canvas);
 		double i = gerDistance(t, canvas);
 		double ii = Math.sqrt(i);
 
+		//根据三边求得cos值
 		double cosA = Math.abs((i + p - o) / (2 * ii * Math.sqrt(p)));
+		//更具cos求sin
 		double sina = Math.sqrt((1 - Math.pow(cosA, 2)));
 
+		//计算投影平面上的2d坐标
 		double xx = ii * sina * (t[0] >= canvas[0] ? 1 : -1);
 		double yy = ii * cosA * (t[1] >= canvas[1] ? 1 : -1);
 
+		//缓存数据
 		b[index][0] = (int) xx;
 		b[index][1] = (int) yy;
 	}
 
-	private double[] aaaaCalPlaneLineIntersectPoint(double[] linePoint) {
+	//计算点经过投影在投影平面的3d坐标
+	private double[] calPlaneLineIntersectPoint(double[] linePoint) {
 		double[] returnResult = new double[3];
 		double vp1, vp2, vp3, n1, n2, n3, v1, v2, v3, m1, m2, m3, t, vpt;
 		// 平面的法线向量
@@ -234,6 +228,7 @@ public class Matrix_3D extends JPanel {
 		return returnResult;
 	}
 
+	//计算两个点之间的间距
 	public double gerDistance(double[] a, double[] b) {
 		double i = 0;
 		for (int j = 0; j < 3; j++) {

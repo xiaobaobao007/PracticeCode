8a9c99f0ea43d94381bdb03f865ea81e03a7809e
xiaobaobao007 <932824098@qq.com>
2020-09-23 21:16:50 +0800

    算法

--- src/Main.java
+++ src/Main.java
@@ -4,39 +4,48 @@
  */
 public class Main {
 	public static void main(String[] args) {
-		System.out.println(get("123_456_789", "456", '_'));
-		System.out.println(get("456_789_123", "456", '_'));
-		System.out.println(get("789_123_456", "456", '_'));
-		System.out.println(get("456", "456", '_'));
-		System.out.println(get("456_", "456", '_'));
-		System.out.println(get("_456", "456", '_'));
+		System.out.println(stringContainsStringBySplit("123_456_789", "456", '_'));
+		System.out.println(stringContainsStringBySplit("456_789_123", "456", '_'));
+		System.out.println(stringContainsStringBySplit("789_123_456", "456", '_'));
+		System.out.println(stringContainsStringBySplit("456", "456", '_'));
+		System.out.println(stringContainsStringBySplit("456_", "456", '_'));
+		System.out.println(stringContainsStringBySplit("_456", "456", '_'));
 		System.out.println();
-		System.out.println(get("45", "456", '_'));
-		System.out.println(get("56", "456", '_'));
-		System.out.println(get("1456", "456", '_'));
-		System.out.println(get("4561", "456", '_'));
-		System.out.println(get("123_4561_789", "456", '_'));
-		System.out.println(get("123_1456_789", "456", '_'));
-		System.out.println(get("4561_789_123", "456", '_'));
-		System.out.println(get("1456_789_123", "456", '_'));
-		System.out.println(get("789_123_1456", "456", '_'));
-		System.out.println(get("789_123_4561", "456", '_'));
+		System.out.println(stringContainsStringBySplit("45", "456", '_'));
+		System.out.println(stringContainsStringBySplit("56", "456", '_'));
+		System.out.println(stringContainsStringBySplit("1456", "456", '_'));
+		System.out.println(stringContainsStringBySplit("4561", "456", '_'));
+		System.out.println(stringContainsStringBySplit("123_4561_789", "456", '_'));
+		System.out.println(stringContainsStringBySplit("123_1456_789", "456", '_'));
+		System.out.println(stringContainsStringBySplit("4561_789_123", "456", '_'));
+		System.out.println(stringContainsStringBySplit("1456_789_123", "456", '_'));
+		System.out.println(stringContainsStringBySplit("789_123_1456", "456", '_'));
+		System.out.println(stringContainsStringBySplit("789_123_4561", "456", '_'));
 	}
 
-	public static boolean get(String a, String b, char c) {
-		int index = a.indexOf(b);
+	/**
+	 * 字符串是否包含另一个字符串，并被分隔符分割
+	 * 用于优化字符串检测时，先转集合，再判断
+	 *
+	 * @param src   被判断的字串
+	 * @param s     是否包含的字符串
+	 * @param split 分隔符
+	 * @return 是否包含的有
+	 */
+	public static boolean stringContainsStringBySplit(String src, String s, char split) {
+		int index = src.indexOf(s);
 		if (index < 0) {
 			return false;
 		}
-		if (a.length() == b.length()) {
+		if (src.length() == s.length()) {
 			return true;
 		}
 		if (index == 0) {
-			return c == a.charAt(index + b.length());
+			return split == src.charAt(index + s.length());
 		}
-		if (index + b.length() == a.length()) {
-			return c == a.charAt(index - 1);
+		if (index + s.length() == src.length()) {
+			return split == src.charAt(index - 1);
 		}
-		return c == a.charAt(index + b.length()) && c == a.charAt(index - 1);
+		return split == src.charAt(index + s.length()) && split == src.charAt(index - 1);
 	}
 }

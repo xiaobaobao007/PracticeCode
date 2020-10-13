a6c0855af7e58b6cc9fc065f2564d45b4470da1f
xiaobaobao007 <932824098@qq.com>
2020-10-13 22:37:00 +0800

    算法

--- src/Arithmetic/LongestValidParentheses.java
+++ src/Arithmetic/LongestValidParentheses.java
@@ -1,7 +1,5 @@
 package Arithmetic;
 
-import java.util.LinkedList;
-
 /**
  * 输入: "(()"
  * 输出: 2
@@ -10,31 +8,40 @@ import java.util.LinkedList;
 public class LongestValidParentheses {
 
 	public static void main(String[] args) {
-		System.out.println(new LongestValidParentheses().longestValidParentheses(")()())"));
+		System.out.println(new LongestValidParentheses().longestValidParentheses("()(()"));
 	}
 
 	public int longestValidParentheses(String s) {
-		int max = 0;
-		int num = 0;
-		boolean go = false;
-		LinkedList<Character> charList = new LinkedList<>();
-		for (char c : s.toCharArray()) {
-			if (!charList.isEmpty() && '(' == charList.getLast() && ')' == c) {
-				if (max < ++num * 2) {
-					max = num * 2;
-					go = true;
+		int maxans = 0;
+		int[] dp = new int[s.length()];
+		for (int i = 1; i < s.length(); i++) {
+			if (s.charAt(i) == ')') {
+				if (s.charAt(i - 1) == '(') {
+					dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
+				} else if (i - dp[i - 1] > 0 && s.charAt(i - dp[i - 1] - 1) == '(') {
+					dp[i] = dp[i - 1] + ((i - dp[i - 1]) >= 2 ? dp[i - dp[i - 1] - 2] : 0) + 2;
 				}
-				charList.removeLast();
-			} else {
-				if (go) {
-					go = false;
-				} else {
-					num = 0;
-				}
-				charList.addLast(c);
+				maxans = Math.max(maxans, dp[i]);
 			}
 		}
-		return max;
+		return maxans;
 	}
 
+	// public int longestValidParentheses(String s) {
+	// 	int n = 0;
+	// 	int m = 0;
+	// 	for (char c : s.toCharArray()) {
+	// 		if ('(' == c) {
+	// 			n++;
+	// 		} else {
+	// 			n--;
+	// 			if (n < 0) {
+	// 				n = 0;
+	// 			} else {
+	// 				m++;
+	// 			}
+	// 		}
+	// 	}
+	// 	return m << 1;
+	// }
 }

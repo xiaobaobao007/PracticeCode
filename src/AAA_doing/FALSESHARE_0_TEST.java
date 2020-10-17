ea50e99b35a1b2c84546f99c1c59eccbf8282113
xiaobaobao007 <932824098@qq.com>
2020-10-18 00:35:08 +0800

    算法

---
+++ src/AAA_doing/FALSESHARE_0_TEST.java
@@ -0,0 +1,54 @@
+package AAA_doing;
+
+public class FALSESHARE_0_TEST implements Runnable {
+	public static int NUM_THREADS = 4;
+	public final static long ITERATIONS = 500L * 1000L * 1000L;
+	private final int arrayIndex;
+	private static VolatileLong[] longs;
+	public static long SUM_TIME = 0l;
+
+	public FALSESHARE_0_TEST(final int arrayIndex) {
+		this.arrayIndex = arrayIndex;
+	}
+
+	public static void main(String[] s) throws Exception {
+		Thread.sleep(10000);
+		for (int j = 0; j < 10; j++) {
+			System.out.println(j);
+			longs = new VolatileLong[NUM_THREADS];
+			for (int i = 0; i < longs.length; i++) {
+				longs[i] = new VolatileLong();
+			}
+			final long start = System.nanoTime();
+			runTest();
+			final long end = System.nanoTime();
+			SUM_TIME += end - start;
+		}
+		System.out.println("平均耗时：" + SUM_TIME / 10);
+	}
+
+	private static void runTest() throws InterruptedException {
+		Thread[] threads = new Thread[NUM_THREADS];
+		for (int i = 0; i < threads.length; i++) {
+			threads[i] = new Thread(new FALSESHARE_0_TEST(i));
+		}
+		for (Thread t : threads) {
+			t.start();
+		}
+		for (Thread t : threads) {
+			t.join();
+		}
+	}
+
+	public void run() {
+		long i = ITERATIONS + 1;
+		while (0 != --i) {
+			longs[arrayIndex].value = i;
+		}
+	}
+
+	public final static class VolatileLong {
+		public volatile long value = 0L;
+		// public long p1, p2, p3, p4, p5, p6;
+	}
+}
